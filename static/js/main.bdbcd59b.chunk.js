ba9fa6bc028093e6bcb9326f5a93e5f27353a6b5
jaeim <68433884+jaeim@users.noreply.github.com>
2021-02-01 05:15:41 +0900

    Updates

--- static/js/main.bdbcd59b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpmovie_app_2021=this.webpackJsonpmovie_app_2021||[]).push([[0],{27:function(t,e,n){"use strict";n.r(e);var a=n(6),c=(n(0),n(14)),o=n.n(c),p=n(15),r=n(1);var s=function(){return Object(a.jsx)("span",{children:"About Page!"})};var i=function(){return Object(a.jsx)(p.a,{children:Object(a.jsx)(r.a,{path:"/about/",component:s})})};o.a.render(Object(a.jsx)(i,{}),document.getElementById("potato"))}},[[27,1,2]]]);
-//# sourceMappingURL=main.bdbcd59b.chunk.js.map
